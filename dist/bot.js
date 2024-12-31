ae9069447bc1d754c412173e953c7aee24cd8ed0
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2024-12-31 19:09:33 +0100

    Set up tsconfig.json to output compiled files to dist/ directory

---
+++ dist/bot.js
@@ -0,0 +1,46 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const grammy_1 = require("grammy");
+const dotenv = __importStar(require("dotenv"));
+// Load environment variables from .env file
+dotenv.config();
+// Use the bot token from the .env file
+const bot = new grammy_1.Bot(process.env.TELEGRAM_BOT_TOKEN);
+bot.command("start", (ctx) => ctx.reply("Welcome to my basic GrammY bot!"));
+bot.on("message", (ctx) => {
+    ctx.reply(`You said: ${ctx.message.text}`);
+});
+bot.start();
